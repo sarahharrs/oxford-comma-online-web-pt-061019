74e530278b3b00f4908735a9b74f8dd32304f08c
Sarah Harrs <sarahharrs@gmail.com>
2019-06-28 13:09:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,6 @@
 def oxford_comma(array)
 array = ["fiddleheads","okra","kohlrabi"]
-result = array.join
-puts array
+return nil if array.nil?
+  return array[0] if array.length == 1
+  return array[0..-2].join(', ') + " and " + array[-1] if array.length > 1
 end
